d256c4d3170c67394d4fc7f84ec6fbbe944c32f3
daguilarm <damian.aguilarm@gmail.com>
2021-05-31 15:10:03 +0200

    refactoring

--- config.py
+++ config.py
@@ -1,14 +1,38 @@
 # Columns
 columns = {
-    'headers': ['ID', 'Usuario', 'Asignatura', 'Preguntas', 'Respuesta 1', 'Respuesta 2', 'Respuesta 3', 'Respuesta 4',
-                'Correcta', 'Tipo', 'Utilizada en...'],
-    'columns': ['id', 'user', 'category', 'question', 'answer_1', 'answer_2', 'answer_3', 'answer_4', 'correct', 'type',
-                'used'],
+    # Table labels
+    'headers': [
+        'ID',
+        'Usuario',
+        'Asignatura',
+        'Preguntas',
+        'Respuesta 1',
+        'Respuesta 2',
+        'Respuesta 3',
+        'Respuesta 4',
+        'Correcta',
+        'Tipo',
+        'Utilizada en...'
+    ],
+    # Database table columns
+    'columns': [
+        'id',
+        'user',
+        'category',
+        'question',
+        'answer_1',
+        'answer_2',
+        'answer_3',
+        'answer_4',
+        'correct',
+        'type',
+        'used'
+    ],
     # Width in %
     'width': ['2', '5', '4', '40', '9', '9', '9', '9', '4', '4', '5'],
 }
 
-# Data base
+# Database defaults
 database = {
     'per_page': 10,
     'page': 1,
